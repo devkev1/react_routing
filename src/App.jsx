803d4e4cd2b27b34b9c2aebe236259695323e962
Kevin Wong <kevingywong@gmail.com>
2021-11-26 23:14:22 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Films from './components/Films';
+import People from './components/People';
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
+
 const App = () => {
-  return <h1>React Router</h1>;
-};
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+          <Routes>
+              <Route path="/home" component={Home}/>
+              <Route path="/films" component={Films}/>
+              <Route path="/people" component={People}/>
+          </Routes>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+const Navbar = () => {
+    return (
+      <nav>
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>Films</li>
+          <li>People</li>
+        </ul>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
+  
