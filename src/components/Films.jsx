696de3a52030eea5480dd077a633b451602012dd
Kevin Wong <kevingywong@gmail.com>
2021-11-30 21:01:32 -0500

    implemented APIs

--- src/components/Films.jsx
+++ src/components/Films.jsx
@@ -1,7 +1,36 @@
+import React, { useState, useEffect } from 'react';
 
 const Films = () => {
+    
+    useEffect(() => {
+        fetchFilms();
+    },[]);
+
+    const [films, setFilms] = useState([]);
+
+    const fetchFilms = async () => {
+        try {
+            const data = await fetch('https://ghibliapi.herokuapp.com/films');
+            const films = await data.json();
+            console.log(films);
+            setFilms(films);
+        } catch (err) {
+            console.log(err)
+            setFilms([])
+        }
+    }
+
     return (
-        <h1>Films</h1>
+        <div>
+            <h1>Films</h1>
+            {films.map(film => (
+                <p key={film.id}>{film.title} 
+                ({film.release_date}) 
+                <br/> RT Score: {film.rt_score}%
+                <br/> Description: {film.description}
+                </p>
+            ))}
+        </div>
     );
   }
   
--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,7 +1,37 @@
+import React, { useState, useEffect } from 'react';
 
 const People = () => {
+
+    useEffect(() => {
+        fetchPeople();
+    },[]);
+
+    const [people, setPeople] = useState([]);
+
+    const fetchPeople = async () => {
+        try {
+            const data = await fetch('https://ghibliapi.herokuapp.com/people');
+            const people = await data.json();
+            console.log(people);
+            setPeople(people);
+        } catch (err) {
+            console.log(err)
+            setPeople([])
+        } 
+    }
+
     return (
-        <h1>People</h1>
+        <div>
+            <h1>People</h1>
+            {people.map(people => (
+                <p key={people.id}>{people.name} {' '}
+                <br/> Gender: {people.gender}
+                <br/> Age: {people.age}
+                <br/> Eye Color: {people.eye_color}
+                <br/> Hair Color: {people.hair_color}
+                </p>
+            ))}
+        </div>
     );
   }
   
